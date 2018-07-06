918c07a7bf95c24e4c5da25434740cab0a4a8a72
Chris Lawton <chris.lawton@torchbox.com>
2018-07-06 22:15:01 +0100

    swap typography themes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,6 @@
 import Typography from 'typography';
-import noriegaTheme from 'typography-theme-noriega'
+import kirkhamTheme from 'typography-theme-kirkham'
 
-const typography = new Typography(noriegaTheme);
+const typography = new Typography(kirkhamTheme);
 
 export default typography;
