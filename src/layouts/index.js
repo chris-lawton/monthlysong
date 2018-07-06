58a57adefa7ad8c9ba8349d63d9055bc50ccf8d5
Chris Lawton <chris.lawton@torchbox.com>
2018-07-06 20:31:55 +0100

    add site title to config than grab it via a query

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-export default ({ children }) => (
+export default ({ children, data }) => (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+        <h1>{data.site.siteMetadata.title}</h1>
         {children()}
     </div>
 );
+
+// get the site title from gatsby-config
+export const query = graphql`
+    query LayoutQuery {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <p>A song you may like, posted once a month.</p>
+    </div>
+)
