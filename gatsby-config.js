1d40176de69bf6966d1120403033a7a78b464f5d
Chris Lawton <chris.lawton@torchbox.com>
2018-07-09 21:25:31 +0100

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,23 @@ module.exports = {
                 pathToConfigModule: `src/utils/typography.js`,
             },
         },
+        {
+            resolve: `gatsby-plugin-favicon`,
+                options: {
+                logo: './favicon.png',
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        }
     ],
 }
