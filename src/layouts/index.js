2f2594d8b6310b04dafa0db8036b5d3e387b1c89
Chris Lawton <chris.lawton@torchbox.com>
2018-07-09 21:17:33 +0100

    add helmet for title tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import { Helmet } from "react-helmet";
 
 export default ({ children, data }) => (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+        <Helmet>
+            <title>{data.site.siteMetadata.title}</title>
+        </Helmet>
         <Link to={'/'} style={{ textDecoration: 'none' }}>
             <h1 style={{margin: `0 0 1rem`}}>{data.site.siteMetadata.title}</h1>
         </Link>
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 export default ({ data }) => {
     const post = data.markdownRemark;
     return (
         <div>
+            <Helmet>
+                <title>{post.frontmatter.title} | MonthlySong</title>
+            </Helmet>
             <h2 style={{ margin: `1rem 0` }}>{post.frontmatter.title}</h2>
             <div dangerouslySetInnerHTML={{__html: post.html}} />
         </div>
