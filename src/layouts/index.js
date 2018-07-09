0ef06bc296268a0eabe92892eb2ce69e6b40bc22
Chris Lawton <chris.lawton@torchbox.com>
2018-07-09 20:40:20 +0100

    add a link back to home

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
 export default ({ children, data }) => (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-        <h1>{data.site.siteMetadata.title}</h1>
+        <Link to={'/'} style={{ textDecoration: 'none' }}>
+            <h1>{data.site.siteMetadata.title}</h1>
+        </Link>
         {children()}
     </div>
 );
