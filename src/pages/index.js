21db07992f9afa89b7d474d8a9f4799fd3c30145
Chris Lawton <chris.lawton@torchbox.com>
2019-02-11 20:29:07 +0000

    move styles to module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import styles from './index.module.css'
+import styles from './index.module.css';
 
 export default ({ data }) => {
     return(
         <div>
             <p>A song you may like, posted once a month.</p>
             {data.allMarkdownRemark.edges.map(({node}) => (
-                <div style={{background: `#eef7b36b`, padding: `1rem`, marginBottom: `1rem`, borderRadius: `4px`, position: `relative`, overflow: `hidden`}} key={node.id}>
-                    <Link to={node.fields.slug} style={{textDecoration: `none`, color: `inherit`}} >
-                        <h3 style={{margin: `0 0 1rem 0`}}>
+                <div className={styles.card} key={node.id}>
+                    <Link to={node.fields.slug}>
+                        <h3 className={styles.cardHeading}>
                             {node.frontmatter.title}
-                            <span
-                                style={{ color: `rgba(255, 255, 255, 0.17)`, fontSize: `120px`, position: `absolute`, right: 0, top: `15px`}}>
+                            <span className={styles.cardDate}>
                                 {node.frontmatter.date}
                             </span>
                         </h3>
-                        <p style={{marginBottom: `0`}}>{node.excerpt}</p>
+                        <p className={styles.cardTeaser}>{node.excerpt}</p>
                     </Link>
                 </div>
             ))}
